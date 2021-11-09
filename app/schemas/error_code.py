4925c386737c528d96b4c8f85f8ab7ce1d3647c5
yijunxu <yijunx@umich.edu>
2021-11-09 15:21:31 +0000

    added some doc string

--- app/schemas/error_code.py
+++ app/schemas/error_code.py
@@ -2,6 +2,9 @@ from enum import Enum
 
 
 class ErrorCodeEnum(int, Enum):
+    """not used yet, the purpose is to return a code,
+    and frontend will render a message based on the code"""
+
     user_login_with_password_fail = 1
     user_login_with_google_fail = 2
     user_register_fail = 3
